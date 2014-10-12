91122f9c843cc95de80725596701f23a01cf47d0
Tim Lovorn <tflovorn@crimson.ua.edu>
2014-10-11 21:01:57 -0500

    implement electron Hamiltonian

--- elHamiltonian.go
+++ elHamiltonian.go
@@ -1,9 +1,67 @@
 package vo2mft
 
 import (
+	"math"
+	"math/cmplx"
+)
+import (
 	"github.com/tflovorn/cmatrix"
+	vec "github.com/tflovorn/scExplorer/vector"
 )
 
-func ElHamiltonian() cmatrix.CMatrix {
-	return nil
+// Calculate 4x4 electronic Hamiltonian.
+// Assumes that k is scaled such that kx*a, ky*a, kz*c --> kx, ky, kz.
+func ElHamiltonian(env *Environment, k vec.Vector) cmatrix.CMatrix {
+	KQ := vec.Vector{math.Pi, math.Pi, math.Pi}
+	k.Add(&KQ) // now KQ = k dot Q
+
+	EpsAE := EpsilonAE(env, k)
+	EpsBE := EpsilonBE(env, k)
+	EpsBE_KQ := EpsilonBE(env, KQ)
+	EpsAO := EpsilonAO(env, k)
+	EpsBO := EpsilonBO(env, k)
+	ikd := complex(0.0, k[0]/2.0 + k[1]/2.0 + k[2]/2.0)
+
+	H := cmatrix.InitSliceCMatrix(4, 4)
+	H[0][0] = 0.5*EpsAE
+	H[1][0] = EpsAO
+	H[2][0] = 0.5*EpsBE*cmplx.Exp(-ikd)
+	H[3][0] = -0.5*cmplx.Conj(EpsBO)*cmplx.Exp(-ikd)
+
+	H[0][1] = -EpsAO
+	H[1][1] = -0.5*EpsAE
+	H[2][1] = 0.5*cmplx.Conj(EpsBO)*cmplx.Exp(-ikd)
+	H[3][1] = complex(0.0, 0.5)*EpsBE_KQ*cmplx.Exp(-ikd)
+
+	H[0][2] = 0.5*EpsBE*cmplx.Exp(ikd)
+	H[1][2] = 0.5*EpsBO*cmplx.Exp(ikd)
+	H[2][2] = 0.5*EpsAE
+	H[3][2] = EpsAO
+
+	H[0][3] = -0.5*EpsBO*cmplx.Exp(ikd)
+	H[1][3] = complex(0.0, -0.5)*EpsBE_KQ*cmplx.Exp(ikd)
+	H[2][3] = -EpsAO
+	H[3][3] = -0.5*EpsAE
+	return H
+}
+
+func EpsilonAE(env *Environment, k vec.Vector) complex128 {
+	rp := -2.0*(env.Tae*(math.Cos(k[0]) + math.Cos(k[1])) + env.Tce*math.Cos(k[2]))
+	return complex(rp, 0.0)
+}
+
+func EpsilonBE(env *Environment, k vec.Vector) complex128 {
+	rp := -8.0*env.Tbe*math.Cos(k[0]/2.0)*math.Cos(k[1]/2.0)*math.Cos(k[2]/2.0)
+	return complex(rp, 0.0)
+}
+
+func EpsilonAO(env *Environment, k vec.Vector) complex128 {
+	ip := -2.0*env.M*(env.Tao*(math.Sin(k[0]) + math.Sin(k[1])) + env.Tco*math.Sin(k[2]))
+	return complex(0.0, ip)
+}
+
+func EpsilonBO(env *Environment, k vec.Vector) complex128 {
+	rp := -8.0*env.M*env.Tbo*math.Cos(k[0]/2.0)*math.Cos(k[1]/2.0)*math.Cos(k[2]/2.0)
+	ip := 8.0*env.M*env.Tbo*math.Sin(k[0]/2.0)*math.Sin(k[1]/2.0)*math.Sin(k[2]/2.0)
+	return complex(rp, ip)
 }
