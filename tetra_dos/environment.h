ebd4b8e299dd52535a853f025f4083a682f249f9
Tim Lovorn <tflovorn@crimson.ua.edu>
2015-08-27 15:55:35 -0500

    keep only necessary vars for H(k) in environment

--- tetra_dos/environment.h
+++ tetra_dos/environment.h
@@ -4,8 +4,6 @@
 #include <stdbool.h>
 
 typedef struct {
-	// Size of BZ on one edge (total number of BZ points is this cubed).
-	int BZPointsPerDim;
 	// Hopping parameters, even symmetry (a, c, diagonal axes).
 	double Tae, Tce, Tbe;
 	// Hopping parameters, odd symmetry (a, c, diagonal axes).
@@ -16,25 +14,8 @@ typedef struct {
 	double W;
 	// Chemical potential.
 	double Mu;
-	// Inverse temperature, 1 / (k_B * T).
-	double Beta;
-	// One-spin term for BEG model: coefficient for (S_i)^2.
-	double B;
-	// Exchange parameters for BEG model: coefficients to S_i dot S_j.
-	// Jb is excluded since it does not contribute to results.
-	double Ja, Jc;
-	// Biquadratic exchange parameters for BEG model: coefficients to (S_i)^2 * (S_j)^2.
-	double Ka, Kc, Kb;
 	// On-site energies in M and R phases.
 	double EpsilonM, EpsilonR;
-	// Consider only ionic part of the problem:
-	// only ions contribute to free energy; should solve
-	// for (M, W).
-	// If this is set to true, need to also set the following to 0:
-	// Tae, Tce, Tbe, Tao, Tco, Tbo, EpsilonM, EpsilonR, Mu.
-	// (maybe don't need to fix Mu = 0 -- large negative value could
-	// be better).
-	bool IonsOnly;
 } Environment;
 
 #endif // TETRA_DOS_ENVIRONMENT
