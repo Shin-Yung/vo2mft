b5894d29ba967607279223d469c25bb4bbf02f12
Tim Lovorn <tflovorn@crimson.ua.edu>
2016-02-23 10:06:10 -0600

    commit Bxz impl fix (no mf on-site) - no qualitative change

--- twodof/ions.go
+++ twodof/ions.go
@@ -11,6 +11,7 @@ func (env *Environment) Z1(Ds *HoppingEV) float64 {
 	val := 0.0
 	for _, S := range all_S {
 		val += math.Exp(-env.Beta * env.H_Ion(S, Ds))
+		//fmt.Println(env.Beta*env.H_Ion(S, Ds), math.Exp(-env.Beta*env.H_Ion(S, Ds)), val)
 	}
 	return val
 }
@@ -46,11 +47,13 @@ func (env *Environment) H_Ion(S []int, Ds *HoppingEV) float64 {
 	Kcxxe, Kczze, Kcxz := 2.0*env.Kcxx(), 2.0*env.Kczz(), env.Kcxz()
 	Dco := Ds.Dco(env)
 
-	S01_part := (Bzz+Bxz*env.W02+Kbe*env.W11+Kczze*env.W01+Kcxz*env.W02)*S01*S01 - (4.0*Jb*env.M11+2.0*Jc*env.M01+2.0*Dco)*S01
-	S11_part := (Bxy+Bxz*env.W12+Kbe*env.W01+Kcxxe*env.W11+Kcxz*env.W12)*S11*S11 - 4.0*Jb*env.M01*S11
-	S02_part := (Bxy+Bxz*env.W01+Kbe*env.W12+Kcxxe*env.W02+Kcxz*env.W01)*S02*S02 - 4.0*Jb*env.M12*S02
-	S12_part := (Bzz+Bxz*env.W11+Kbe*env.W02+Kczze*env.W12+Kcxz*env.W11)*S12*S12 - (4.0*Jb*env.M02+2.0*Jc*env.M12+2.0*Dco)*S12
-	return S01_part + S11_part + S02_part + S12_part
+	S01_part := (Bzz+Kbe*env.W11+Kczze*env.W01+Kcxz*env.W02)*S01*S01 - (4.0*Jb*env.M11+2.0*Jc*env.M01+2.0*Dco)*S01
+	S11_part := (Bxy+Kbe*env.W01+Kcxxe*env.W11+Kcxz*env.W12)*S11*S11 - 4.0*Jb*env.M01*S11
+	S02_part := (Bxy+Kbe*env.W12+Kcxxe*env.W02+Kcxz*env.W01)*S02*S02 - 4.0*Jb*env.M12*S02
+	S12_part := (Bzz+Kbe*env.W02+Kczze*env.W12+Kcxz*env.W11)*S12*S12 - (4.0*Jb*env.M02+2.0*Jc*env.M12+2.0*Dco)*S12
+	S02_S01_part := Bxz * S02 * S02 * S01 * S01
+	S11_S12_part := Bxz * S11 * S11 * S12 * S12
+	return S01_part + S11_part + S02_part + S12_part + S02_S01_part + S11_S12_part
 }
 
 // Constant part of the ionic Hamiltonian (no S dependence).
@@ -59,8 +62,7 @@ func (env *Environment) EConst_Ion() float64 {
 	dimer_quart := -env.Kcxx()*(env.W02*env.W02+env.W11*env.W11) + env.Kczz()*(env.W01*env.W01+env.W12*env.W12) + env.Kcxz()*(env.W02*env.W01+env.W11*env.W12)
 	cb := 4.0 * env.Jb() * (env.M01*env.M11 + env.M02*env.M12)
 	ccbb := -4.0 * env.Kb() * (env.W01*env.W11 + env.W02*env.W12)
-	onsite := -env.Bxz() * (env.W01*env.W02 + env.W11*env.W12)
-	return dimer_quad + dimer_quart + cb + ccbb + onsite
+	return dimer_quad + dimer_quart + cb + ccbb
 }
 
 // Constant part of the electron-ion Hamiltonian.
