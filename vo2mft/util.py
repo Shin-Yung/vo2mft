992d430a11fc6faced791e2863c978b7a8b32d17
Tim Lovorn <tflovorn@austin.utexas.edu>
2016-11-23 16:21:19 -0600

    simplify path determination inside repo

--- vo2mft/util.py
+++ vo2mft/util.py
@@ -1,46 +1,20 @@
 import os
+import inspect
+
+def _base_dir():
+    frame = inspect.getfile(inspect.currentframe())
+    this_dir = os.path.dirname(os.path.abspath(frame))
+    base_dir = os.path.join(this_dir, "..")
+    return os.path.normpath(base_dir)
 
 def _solve_front_path():
-    # TODO - avoid assumption that pwd = vo2mft/vo2mft.
-    # Can use $GOPATH (in general need to consider multiple entries (separated
-    # by color).
-    # TODO - get from gopath/bin instead?
-    config_path = "config"
-    config = _load_config(config_path)
-    return os.path.join(config["base_path"], "vo2solve", "vo2solve_front", "vo2solve_front")
+    return os.path.join(_base_dir(), "vo2solve", "vo2solve_front", "vo2solve_front")
 
 def _twodof_solve_front_path():
-    # TODO - avoid assumption that pwd = vo2mft/vo2mft.
-    # Can use $GOPATH (in general need to consider multiple entries (separated
-    # by color).
-    # TODO - get from gopath/bin instead?
-    config_path = "config"
-    config = _load_config(config_path)
-    return os.path.join(config["base_path"], "twodof", "vo2solve_front", "vo2solve_front")
+    return os.path.join(_base_dir(), "twodof", "vo2solve_front", "vo2solve_front")
 
 def _twodof_body_fixed_solve_front_path():
-    # TODO - avoid assumption that pwd = vo2mft/vo2mft.
-    # Can use $GOPATH (in general need to consider multiple entries (separated
-    # by color).
-    # TODO - get from gopath/bin instead?
-    config_path = "config"
-    config = _load_config(config_path)
-    return os.path.join(config["base_path"], "twodofavg", "vo2solve_front", "vo2solve_front")
+    return os.path.join(_base_dir(), "twodofavg", "vo2solve_front", "vo2solve_front")
 
 def _run_dos_path():
-    # TODO - avoid assumption that pwd = vo2mft/vo2mft.
-    config_path = "config"
-    config = _load_config(config_path)
-    return os.path.join(config["base_path"], "tetra_dos", "RunDosValues.out")
-
-def _load_config(config_path):
-    config = {}
-    with open(config_path, 'r') as fp:
-        lines = fp.readlines()
-        for line in lines:
-            lstr = line.strip()
-            first_space = lstr.index(' ')
-            k = lstr[:first_space]
-            v = lstr[first_space+1:]
-            config[k] = v
-    return config
+    return os.path.join(_base_dir(), "tetra_dos", "RunDosValues.out")
